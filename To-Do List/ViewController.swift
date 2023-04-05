a6a98c19de5ca60947ed9533c8719e5058c9398e
Bakhtovar <bakhtovar.github@gmail.com>
2023-04-05 14:32:06 +0600

    Initial Commit

---
+++ To-Do List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Bakhtovar Umarov on 05/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
